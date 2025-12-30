c34a81c6c7480166f474bed5a0f1454fa4e32e52
adri-capsitech <adri.wadadar@capsitech.com>
2025-12-30 20:01:36 +0530

    Fixed the force logic and camera movement and pendulum adjustment

---
+++ Assets/DumpIt/Scripts/SpawnCollider.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class SpawnCollider : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Car"))
+        {
+            Debug.Log("Spawn Collider hit by " + other.name);
+            GameMechanics.Instance.SpawnCars();
+            GameMechanics.Instance.AdjustHeight();
+        }
+    }
+}
