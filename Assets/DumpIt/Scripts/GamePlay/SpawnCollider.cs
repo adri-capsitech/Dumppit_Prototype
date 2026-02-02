a781b92f5b053f74cfe4e692a85bc304b2804623
adri-capsitech <adri.wadadar@capsitech.com>
2026-02-02 11:11:01 +0530

    Minor fixes

--- Assets/DumpIt/Scripts/GamePlay/SpawnCollider.cs
+++ Assets/DumpIt/Scripts/GamePlay/SpawnCollider.cs
@@ -1,13 +1,40 @@
+// using UnityEngine;
+
+// public class SpawnCollider : MonoBehaviour
+// {
+//     private void OnTriggerEnter(Collider other)
+//     {
+//         if (other.CompareTag("Car"))
+//         {
+//             GameMechanics.Instance.SpawnCars();
+//             GameMechanics.Instance.AdjustHeight();
+//         }
+//     }
+// }
+
+
 using UnityEngine;
 
 public class SpawnCollider : MonoBehaviour
 {
+    private bool heightAdjusted = false;
+
     private void OnTriggerEnter(Collider other)
     {
+        if (heightAdjusted) return;
+
         if (other.CompareTag("Car"))
         {
-            GameMechanics.Instance.SpawnCars();
+            heightAdjusted = true;
+
             GameMechanics.Instance.AdjustHeight();
+            GameMechanics.Instance.SpawnCars();
         }
+    }
+
+    // Reset when a new car is spawned / new level starts
+    public void ResetTrigger()
+    {
+        heightAdjusted = false;
     }
 }
