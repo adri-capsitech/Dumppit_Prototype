f5cd72a5a36ed26f48cd35dcc29b65cdfed82b49
adri-capsitech <adri.wadadar@capsitech.com>
2026-02-09 13:26:15 +0530

    setting panel image and bg

--- Assets/DumpIt/Scripts/Audio/UI/SettingsUI.cs
+++ Assets/DumpIt/Scripts/Audio/UI/SettingsUI.cs
@@ -5,22 +5,27 @@ public class SettingsUI : MonoBehaviour
 {
     [SerializeField] private Button backButton;
     [SerializeField] private Button privacyPolicyButton;
+    public Image imagepanel;
+
     private void OnEnable()
     {
         backButton.onClick.AddListener(OnBackClicked);
         privacyPolicyButton.onClick.AddListener(OnPrivacyPolicyClicked);
+        imagepanel.enabled = true;
     }
 
     private void OnDisable()
     {
         backButton.onClick.RemoveListener(OnBackClicked);
         privacyPolicyButton.onClick.RemoveListener(OnPrivacyPolicyClicked);
+        imagepanel.enabled = false;
     }
 
     public void OnBackClicked()
     {
-          AppStateManager.Instance.HideOverlay("Settings");
+        AppStateManager.Instance.HideOverlay("Settings");
         AppStateManager.Instance.SetHome();
+        imagepanel.enabled = false;
     }
 
     void OnPrivacyPolicyClicked()
